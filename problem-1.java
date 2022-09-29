bafb9eaa10272f4886029bbc162f693f46788e59
wajhul786 <114664545+wajhul786@users.noreply.github.com>
2022-09-29 20:21:56 +0530

    Create problem-1.java

---
+++ problem-1.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+public class Calculator
+{
+    public static void main(String[] args) 
+    {
+        double a, b,add, sub, mul,div;
+         
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter first number:");
+        a = s.nextInt();
+        System.out.print("Enter second number:");
+        b = s.nextInt();
+        while(true)
+        {
+            System.out.println("Enter 1 for addition");
+            System.out.println("Enter 2 for subtraction");
+            System.out.println("Enter 3 for multiplication");
+            System.out.println("Enter 4 for division");
+            System.out.println("Enter 5 to Exit");
+            int opt = s.nextInt();
+            switch(opt)
+            {
+                case 1:
+                add = a + b;
+                System.out.println("Result:"+add);
+                break;
+ 
+                case 2:
+                sub = a - b;
+                System.out.println("Result:"+sub);
+                break;
+ 
+                case 3:
+                mul = a * b;
+                System.out.println("Result:"+mul);
+                break;
+ 
+                case 4:
+                div = (double)a/b;
+                System.out.println("Result:"+div);
+                break;    
+ 
+                case 5:
+                System.exit(0);
+            }
+        }
+    }
+}
